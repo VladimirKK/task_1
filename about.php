f6b9ad44bf207de468039721596b15e417dfc48a
VladimirKK <42947905+VladimirKK@users.noreply.github.com>
2018-12-08 12:24:34 +0300

    создание базовой разметки
    создание базовой разметки

---
+++ about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $user_name = 'Владимир Казаков'
+
+    ?>
+  </body>
+</html>
